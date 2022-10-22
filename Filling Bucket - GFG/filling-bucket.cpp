0bb9e5c496935721effbe5126fae3cdbf1c82f0b
sourav <89222661+Souravgne@users.noreply.github.com>
2022-10-22 23:04:17 +0530

    Added solution - LeetHub

--- Filling Bucket - GFG/filling-bucket.cpp
+++ Filling Bucket - GFG/filling-bucket.cpp
@@ -5,24 +5,15 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  
   int mod = 1e8;
   int solve(int N , vector<int>& ans){
-      
-      
-      if(N==0)return 1;
       if(N<0)return 0;
-     if(ans[N] != -1){
-         return ans[N];
-     }
-    int ans1 =0 , ans2 =0;
-    ans1+=solve(N-1,ans);
-    ans2+=solve(N-2,ans);
-    
-    return ans[N] = (ans1+ans2)%mod;
+      if(N== 0)return 1;
+      if(ans[N]!=-1)return ans[N];
+      return ans[N]=(solve(N-1, ans)+solve(N-2,ans))%mod;
   }
     int fillingBucket(int N) {
-    vector<int> ans(N+1 ,-1);
+    vector<int> ans(N+1 , -1);
     return solve(N, ans);
     }
 };
