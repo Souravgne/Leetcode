1d37d393ed91e3a40512c2597856a0b3c08126e2
sourav <89222661+Souravgne@users.noreply.github.com>
2023-03-02 10:59:44 +0530

    Time: 18 ms (31.70%), Space: 20.8 MB (8.73%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,27 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-        int height(TreeNode* root, int&d){
-                if(root== NULL)return 0;
-              int left = height(root->left ,d);
-              int right = height(root->right,d);
-                d= max(d, left+right);
-                return max(left,right)+1;
+    pair<int,int> diameter(TreeNode* root){
+        if(root==NULL){
+          pair<int,int> p = make_pair(0,0);
+            return p;
         }
-    int diameterOfBinaryTree(TreeNode* root) {
-           int d =0 ; 
-            height(root, d);
-            return d;
+        pair<int,int> left = diameter(root->left);
+        pair<int,int> right = diameter(root->right);
         
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second+1 ;
+        
+        pair<int,int> ans;
+        ans.first = max(op1, max(op2,op3));
+        ans.second = max(left.second , right.second)+1;
+        return ans;  
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        return diameter(root).first-1;
     }
 };
