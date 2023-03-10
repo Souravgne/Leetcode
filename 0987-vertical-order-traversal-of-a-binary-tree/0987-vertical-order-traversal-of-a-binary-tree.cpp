0cf90bb66363fddbe1d5f2a5f19d223067edc144
sourav <89222661+Souravgne@users.noreply.github.com>
2023-03-10 19:53:01 +0530

    Time: 7 ms (53.33%), Space: 13.7 MB (19.33%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,39 +1,39 @@
+
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int , map<int,multiset<int> > > nodes;
-        queue<pair<TreeNode* , pair<int, int> > > q;
-        vector<vector<int>> ans;
-        
+         vector<vector<int>> ans;
+        map<int, map<int , multiset<int> > >node;
+        queue<pair<TreeNode* , pair<int ,int> > > q;
         q.push(make_pair(root, make_pair(0,0)));
         
         while(!q.empty()){
-            pair<TreeNode* , pair<int, int> >  temp =q.front();
+            pair<TreeNode* , pair<int, int>> temp=q.front();
             q.pop();
             TreeNode* frontNode = temp.first;
-            int hd= temp.second.first;
+            int hd = temp.second.first;
             int lvl= temp.second.second;
             
-            nodes[hd][lvl].insert(frontNode->val);
+            node[hd][lvl].insert(frontNode->val);
+            if(frontNode->left){  q.push(make_pair(frontNode->left, make_pair(hd-1, lvl+1)));}
+            if(frontNode->right){q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));}
             
-            if(frontNode->left){
-                q.push(make_pair(frontNode->left , make_pair(hd-1 , lvl+1)));
-            }
+           
             
-            if(frontNode->right){
-                q.push(make_pair(frontNode->right , make_pair(hd+1 , lvl+1)));
-            }
+
         }
         
-        for(auto i: nodes){
-            vector<int> level;
-            for(auto j: i.second){
-                for(auto k: j.second){
-                    level.push_back(k);
+         for(auto i:node){ 
+             vector<int> v;
+                for(auto j:i.second){ 
+                    
+                    for(auto k: j.second){
+                    v.push_back(k);
+                    }
+                    
                 }
+                ans.push_back(v);
             }
-            ans.push_back(level);
-        }
         return ans;
     }
 };
