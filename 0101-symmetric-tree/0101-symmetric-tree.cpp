d3724073ac4a61eb00a031a2197f4cb87844261b
sourav <89222661+Souravgne@users.noreply.github.com>
2023-03-13 10:24:27 +0530

    Time: 0 ms (100.00%), Space: 16.3 MB (96.56%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,19 +1,38 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-        bool check(TreeNode* root1 , TreeNode* root2){
-               if(root1== NULL && root2==NULL)return true;
-                if(root1== NULL && root2 != NULL)return false;
-                if(root1!=NULL&& root2== NULL)return false;
-                if(root1->val!=  root2->val)return false;
-                
-                bool op1 = check(root1->left ,root2->right);
-                bool op2 = check(root1->right , root2->left);
-                return op1&& op2;
-        }
+    bool solve(TreeNode* root1 , TreeNode* root2){
+      if(root1==NULL && root2==NULL)return true;
+        if(root1 ==NULL && root2!=NULL)return false;
+        if(root1!=NULL && root2==NULL)return false;
+           if(root1->val != root2->val)
+            return false;
+       
+        
+      
+       
+       
+ bool left =solve(root1->left , root2->right);
+ bool right= solve(root1->right , root2->left);
+         
+       
+  
+        return left && right;
+    }
     bool isSymmetric(TreeNode* root) {
-   
-                    return check(root->left , root->right);
-
+       
+       return  solve(root->left , root->right);
+        
+        
     }
 };
