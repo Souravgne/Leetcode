ed3c432b66aba8208dbd38c9545c0174e39cb138
sourav <89222661+Souravgne@users.noreply.github.com>
2023-03-16 20:25:48 +0530

    Time: 88 ms (6.69%), Space: 222.1 MB (5.11%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,37 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int &index ,int inorderStart ,int inorderEnd, map<int, int>&mp){
+    
+    TreeNode* solve(vector<int> inorder, vector<int> postorder , int &index , int inorderStart , int inorderEnd,map<int , int>& mp){
+        if(index <0  || inorderStart> inorderEnd){
+            return NULL;
+        }
         
-        if(index<0 || inorderStart> inorderEnd){
-return NULL;}
-        int element = postorder[index--];
-        TreeNode* root = new TreeNode(element);
-        int pos = mp[element];
-         root->right= solve(inorder, postorder, index , pos+1 , inorderEnd, mp);
-        
-        root->left = solve(inorder, postorder ,index , inorderStart , pos-1 , mp);
-       
+        int value = postorder[index--];
+        TreeNode* root = new TreeNode(value);
+        int temp = mp[value];
+        root->right = solve(inorder , postorder, index , temp+1 , inorderEnd , mp );
+         root->left = solve(inorder , postorder, index , inorderStart , temp-1 , mp );
         return root;
+        
+        
+        
     }
     
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int index = postorder.size()-1  ;
-        map<int, int> mp;
-        for(int i =0 ;i< inorder.size();i++){
-            mp[inorder[i]]= i;
+        map<int , int> mp;
+        for(int i =0 ; i<inorder.size();i++){
+            mp[inorder[i]] = i;
         }
-        TreeNode* ans = solve(inorder, postorder, index , 0 , inorder.size()-1  ,mp);
-        return ans;
+        
+      int  postOrderIndex =postorder.size()-1;
+      return  solve(inorder , postorder, postOrderIndex , 0 , inorder.size()-1, mp);
+        
     }
 };
