39b77455d5d2ee3b21c34496de7be1d5e255c668
sourav <89222661+Souravgne@users.noreply.github.com>
2023-03-26 19:44:08 +0530

    Time: 3 ms (60.08%), Space: 8.3 MB (10.64%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sum = 0;
+    void reverseInorder(TreeNode* root) {
+        if (!root) return;
+        reverseInorder(root->right);
+        root->val += sum;
+        sum = root->val;
+        reverseInorder(root->left);
+    }
+    void inorder(TreeNode* root, vector<int>& ans) {
+        if (!root) return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        if (!root) return NULL;
+        vector<int> ans;
+        inorder(root, ans);
+        reverseInorder(root);
+        return root;
+    }
+};
